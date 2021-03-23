aa6b2839de6029b78b9e6468845dab159c08442d
selezenss <selezenss@gmail.com>
2021-03-23 15:52:30 +0200

    [ss] solving 03-4-uniq

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,14 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
+export function uniq(arr = []) {
+  const result = [];
 
+  arr.forEach(function (item) {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  });
+
+  return result;
 }
